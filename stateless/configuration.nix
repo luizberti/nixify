f43ef2c8a168007ff85c3462dd7ac3bf47bdc44f
Luiz Berti <luizberti@users.noreply.github.com>
2021-07-18 16:50:57 -0300

    Enhance configuration.nix

--- stateless/configuration.nix
+++ stateless/configuration.nix
@@ -9,21 +9,41 @@
     time.timeZone = "England/London";
     i18n.defaultLocale = "en_US.UTF-8";
 
+
+    # =======================
+    # NETWORKING AND SERVICES
+    # =======================
+
     networking.hostName = "stateless";
     networking.interfaces.enp1s0.useDHCP = true;
     # networking.wireless.enable = true;
     networking.useDHCP = false;  # this is deprecated so we set to false
 
     networking.firewall.enable = true;
-    networking.firewall.allowedTCPPorts = [ 22 ];
-    networking.firewall.allowedUDPPorts = [];
+    networking.firewall.allowedTCPPorts = [ 22 ];     # SSH
+    networking.firewall.allowedUDPPorts = [ 51820 ];  # WireGuard
 
     services.openssh.enable = true;
 
+
+    # ===============
+    # USER MANAGEMENT
+    # ===============
+
+    users.mutableUsers = false;
+    users.users.root.hashedPassword = "disabled";
+
     users.users.operator = {
+        home = "/home/operator";
         isNormalUser = true;
-        extraGroups = [ "wheel" ];
+        extraGroups = [ "wheel" "networkmanager" ];
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPSYQ1YyGabCnHTKVYgBxnAhalznCHIpg1V6/Wo5dBS" ];
     };
+
+
+    # ==================
+    # PACKAGE MANAGEMENT
+    # ==================
 
     environment.systemPackages = with pkgs; [
         vim
