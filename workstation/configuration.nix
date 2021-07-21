c28d99f1e5733776a7a74865c22c978875f2f1d7
Luiz Berti <luizberti@users.noreply.github.com>
2021-07-21 09:45:29 -0300

    Add a bunch of configs

--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -3,11 +3,47 @@
 {
     imports = [ ./hardware-configuration.nix ];
 
+    security.protectKernelImage = true;
+
     system.stateVersion = "21.05";
+    system.autoUpgrade.enable = true;
+    system.autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable";
+
+    nix.gc.automatic = true;
+    nix.gc.dates = "03:15";
 
     boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.editor = false;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.cleanTmpDir = true;
+    boot.tmpOnTmpfs = true;
+    boot.kernelModules = [ "tcp_bbr" ];
+    boot.kernel.sysctl = {
+        "kernel.sysrq" = 0;
+
+        # mitigate IP spoofing with reverse path filtering
+        "net.ipv4.conf.default.rp_filter" = 1;
+        "net.ipv4.conf.all.rp_filter" = 1;
+
+        # mitigates MITM by refusing ICMP redirects
+        "net.ipv4.icmp_ignore_bogus_error_responses" = 1;  # prevents logs from filling up
+        "net.ipv4.conf.all.accept_redirects" = 0;
+        "net.ipv4.conf.default.accept_redirects" = 0;
+        "net.ipv4.conf.all.secure_redirects" = 0;
+        "net.ipv4.conf.default.secure_redirects" = 0;
+        "net.ipv6.conf.all.accept_redirects" = 0;
+        "net.ipv6.conf.default.accept_redirects" = 0;
+
+        "net.ipv4.tcp_syncookies" = 1;  # protects against SYN floods
+        "net.ipv4.tcp_rfc1337" = 1;     # somewhat protects against TIME-WAIT assassination
+
+        "net.ipv4.tcp_fastopen" = 3;
+        "net.ipv6.tcp_fastopen" = 3;
+
+        "net.ipv4.tcp_congestion_control" = "bbr";
+        "net.ipv6.tcp_congestion_control" = "bbr";
+        "net.core.default_qdisc" = "cake";
+    };
 
     time.timeZone = "America/Sao_Paulo";
     i18n.defaultLocale = "en_US.UTF-8";
@@ -25,12 +61,25 @@
     networking.interfaces.enp1s0.useDHCP = true;
     networking.useDHCP = false;  # this is deprecated so we set to false
 
+    networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
+
     networking.firewall.enable = true;
+    networking.firewall.allowPing = true;
     networking.firewall.allowedTCPPorts = [ 22 ];     # SSH
     networking.firewall.allowedUDPPorts = [ 51820 ];  # WireGuard
 
+    networking.wireguard.interfaces.wg0 = {
+        ips = [ "100.64.0.1/10" ];
+        privateKeyFile = "/linger/etc/wireguard/private.key";
+
+        # peers = [];
+    };
+
     services.openssh.enable = true;
     services.openssh.permitRootLogin = "no";
+    services.openssh.passwordAuthentication = false;
+
+    security.acme.acceptTerms = true;
 
 
     # ===============
@@ -59,6 +108,7 @@
 
     nixpkgs.config.allowUnfree = true;
 
+    environment.shells = [ pkgs.fish ];
     environment.systemPackages = with pkgs; [
         dash
         fish
